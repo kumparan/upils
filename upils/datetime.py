88cca6ecbab3357b11101ea5d9d35352dedaaa25
Niti Utami <niti.utami@kumparan.com>
2023-11-03 10:34:04 +0700

    datetime to timestamp millis

--- upils/datetime.py
+++ upils/datetime.py
@@ -1,5 +1,6 @@
 """Module providing list of function related to date"""
 from datetime import datetime
+from typing import Union
 
 import pytz
 
@@ -18,3 +19,23 @@ def to_utc7(date: datetime) -> datetime:
     utc7_timezone = pytz.timezone("Asia/Jakarta")
 
     return date.astimezone(tz=utc7_timezone)
+
+
+def datetime_to_timestamp_millis(dt: Union[datetime, str]) -> int:
+    """Converting datetime to unix timestamp without changing timezone.
+
+    Args:
+        dt: datetime object or str that will be converted.
+
+    Returns:
+        Timestamp (millisecond)
+    """
+    if isinstance(dt, str):
+        dt = datetime.fromisoformat(dt)
+    if isinstance(dt, datetime):
+        if dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None:
+            # remove timezone awareness from timestamptz column value
+            dt = dt.replace(tzinfo=None)
+
+    epoch = datetime.utcfromtimestamp(0)
+    return (dt - epoch).total_seconds() * 1000
