2190f3a3645ed6167c0b98eaf95b3e8b5f7ffc0a
Moh Achun Armando <mohamad.armando@kumparan.com>
2024-05-22 13:57:41 +0700

    feature: add get_unique_list

---
+++ tests/test_list.py
@@ -0,0 +1,21 @@
+import unittest
+
+from upils import list as upils_list
+
+
+class ListCase(unittest.TestCase):
+    def test_string_list(self):
+        list_input = ["1", "1", "2", "2", "3"]
+        expected = ["1", "2", "3"]
+        res = upils_list.get_unique_list(list_input)
+        self.assertCountEqual(res, expected)
+
+    def test_num_list(self):
+        list_input = [1, 2, 2, 2, 3, 3]
+        expected = [1, 2, 3]
+        res = upils_list.get_unique_list(list_input)
+        self.assertCountEqual(res, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
