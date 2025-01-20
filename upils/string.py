87f56faf6bdf3baaaa1a6b76d9d9bf2a4297f812
Niti Utami <niti.utami@kumparan.com>
2025-01-20 11:30:04 +0700

    add feature

---
+++ upils/string.py
@@ -0,0 +1,10 @@
+from base64 import b64encode
+import hashlib
+from typing import Callable
+
+
+def get_base64_string_from_hash(
+    data: str, hash_function: Callable = hashlib.sha256
+) -> str:
+    """Get base64 string from hash digest. Used in surrogate key generation."""
+    return b64encode(hash_function(data.encode()).digest()).decode()
