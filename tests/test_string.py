8292ec59c8fb686e10f75523a1646a460685b6ef
Niti Utami <niti.utami@kumparan.com>
2025-01-20 13:26:20 +0700

    rename function

--- tests/test_string.py
+++ tests/test_string.py
@@ -5,10 +5,10 @@ from upils import string as upils_string
 
 
 class StringCase(unittest.TestCase):
-    def test_string_list(self):
+    def test_hash_and_encode_to_base64(self):
         expected_base64_string = "gStIZtdCBFym9YM884kKewVZE3Vw8voFmVe1tMr2pRo="
         string_data = "test_string_to_hash"
-        actual_base64_string = upils_string.get_base64_string_from_hash(
+        actual_base64_string = upils_string.hash_and_encode_to_base64(
             data=string_data, hash_function=hashlib.sha256
         )
         self.assertEqual(expected_base64_string, actual_base64_string)
--- upils/string.py
+++ upils/string.py
@@ -3,7 +3,7 @@ from base64 import b64encode
 from typing import Callable
 
 
-def get_base64_string_from_hash(
+def hash_and_encode_to_base64(
     data: str, hash_function: Callable = hashlib.sha256
 ) -> str:
     """Get base64 string from hash digest. Used in surrogate key generation."""
