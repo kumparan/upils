2eac3e61afb9e3cdeaeb7e957cafb78e87a05c6e
Nabil Mafaza <nabil.mafaza@kumparan.com>
2025-09-02 10:41:07 +0700

    feature: add slate converter module

--- upils/string.py
+++ upils/string.py
@@ -1,3 +1,5 @@
+"""Module to process and transform strings."""
+
 import hashlib
 from base64 import b64encode
 from typing import Callable
@@ -8,6 +10,7 @@ def hash_and_encode_to_base64(
 ) -> str:
     """Get base64 string from hash digest. Used in surrogate key generation."""
     return b64encode(hash_function(data.encode()).digest()).decode()
+
 
 def stringify_value(value: str | None, replacement_value: str = "NULL") -> str:
     """Stringify value to use in SQL INSERT statement"""
